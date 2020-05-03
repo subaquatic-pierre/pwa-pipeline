a117ac6b387b8e4175f8dda356ea031b684f692e
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-05-03 15:26:38 +0400

    fi

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
